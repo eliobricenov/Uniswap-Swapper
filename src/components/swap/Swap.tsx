2508887310b6bd86c137b2afcbe3ba79a728fd45
Elio <eliobricenov@gmail.com>
2021-01-21 22:38:26 -0400

    add provider

--- src/components/swap/Swap.tsx
+++ src/components/swap/Swap.tsx
@@ -1,4 +1,5 @@
-import React, {FC, useCallback, useEffect, useState} from 'react';
+import React, { FC, useCallback, useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 import {
   ChainId,
   CurrencyAmount,
@@ -12,9 +13,14 @@ import {
   Trade,
   TradeType,
 } from '@uniswap/sdk';
-import formattedPriceImpact from "../FormattedPriceImpact";
+import formattedPriceImpact from '../FormattedPriceImpact';
 
 const chainId = ChainId.MAINNET;
+
+const provider = new ethers.providers.JsonRpcProvider(
+  'https://eth-mainnet.alchemyapi.io/v2/iAR-_vVVMF16FADwItPv7SEayXW0-Ee9',
+);
+
 const inputRegex = RegExp(`^\\d*(?:\\\\[.])?\\d*$`);
 const escapeRegExp = (input: string) => {
   return input.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
@@ -99,44 +105,62 @@ const initialTradeState: TradeState = {
   realizedLPFee: null,
 };
 
-const BASE_FEE = new Percent(JSBI.BigInt(30), JSBI.BigInt(10000))
-const ONE_HUNDRED_PERCENT = new Percent(JSBI.BigInt(10000), JSBI.BigInt(10000))
-const INPUT_FRACTION_AFTER_FEE = ONE_HUNDRED_PERCENT.subtract(BASE_FEE)
+const BASE_FEE = new Percent(JSBI.BigInt(30), JSBI.BigInt(10000));
+const ONE_HUNDRED_PERCENT = new Percent(JSBI.BigInt(10000), JSBI.BigInt(10000));
+const INPUT_FRACTION_AFTER_FEE = ONE_HUNDRED_PERCENT.subtract(BASE_FEE);
 
 function computeTradePriceBreakdown(
-  trade?: Trade | null
-): { priceImpactWithoutFee: Percent | undefined; realizedLPFee: CurrencyAmount | undefined | null } {
+  trade?: Trade | null,
+): {
+  priceImpactWithoutFee: Percent | undefined;
+  realizedLPFee: CurrencyAmount | undefined | null;
+} {
   // for each hop in our trade, take away the x*y=k price impact from 0.3% fees
   // e.g. for 3 tokens/2 hops: 1 - ((1 - .03) * (1-.03))
   const realizedLPFee = !trade
     ? undefined
     : ONE_HUNDRED_PERCENT.subtract(
-      trade.route.pairs.reduce<Fraction>(
-        (currentFee: Fraction): Fraction => currentFee.multiply(INPUT_FRACTION_AFTER_FEE),
-        ONE_HUNDRED_PERCENT
-      )
-    )
+        trade.route.pairs.reduce<Fraction>(
+          (currentFee: Fraction): Fraction =>
+            currentFee.multiply(INPUT_FRACTION_AFTER_FEE),
+          ONE_HUNDRED_PERCENT,
+        ),
+      );
 
   // remove lp fees from price impact
-  const priceImpactWithoutFeeFraction = trade && realizedLPFee ? trade.priceImpact.subtract(realizedLPFee) : undefined
+  const priceImpactWithoutFeeFraction =
+    trade && realizedLPFee
+      ? trade.priceImpact.subtract(realizedLPFee)
+      : undefined;
 
   // the x*y=k impact
   const priceImpactWithoutFeePercent = priceImpactWithoutFeeFraction
-    ? new Percent(priceImpactWithoutFeeFraction?.numerator, priceImpactWithoutFeeFraction?.denominator)
-    : undefined
+    ? new Percent(
+        priceImpactWithoutFeeFraction?.numerator,
+        priceImpactWithoutFeeFraction?.denominator,
+      )
+    : undefined;
 
   // the amount of the input that accrues to LPs
   const realizedLPFeeAmount =
     realizedLPFee &&
     trade &&
     (trade.inputAmount instanceof TokenAmount
-      ? new TokenAmount(trade.inputAmount.token, realizedLPFee.multiply(trade.inputAmount.raw).quotient)
-      : CurrencyAmount.ether(realizedLPFee.multiply(trade.inputAmount.raw).quotient))
+      ? new TokenAmount(
+          trade.inputAmount.token,
+          realizedLPFee.multiply(trade.inputAmount.raw).quotient,
+        )
+      : CurrencyAmount.ether(
+          realizedLPFee.multiply(trade.inputAmount.raw).quotient,
+        ));
 
-  return {priceImpactWithoutFee: priceImpactWithoutFeePercent, realizedLPFee: realizedLPFeeAmount}
+  return {
+    priceImpactWithoutFee: priceImpactWithoutFeePercent,
+    realizedLPFee: realizedLPFeeAmount,
+  };
 }
 
-const Swap: FC<Props> = ({origin, target}: Props) => {
+const Swap: FC<Props> = ({ origin, target }: Props) => {
   const [loading, setLoading] = useState(false);
   const [swapInformation, setSwapInformation] = useState(initialSwapState);
   const [tradeInformation, setTradeInformation] = useState(initialTradeState);
@@ -146,7 +170,7 @@ const Swap: FC<Props> = ({origin, target}: Props) => {
       setLoading(true);
       const originToken = new Token(chainId, origin.address, origin.decimals);
       const targetToken = new Token(chainId, target.address, target.decimals);
-      const pair = await Fetcher.fetchPairData(originToken, targetToken);
+      const pair = await Fetcher.fetchPairData(originToken, targetToken, provider);
       const originToTarget = new Route([pair], originToken);
       const targetToOrigin = new Route([pair], targetToken);
       setSwapInformation({
@@ -173,12 +197,12 @@ const Swap: FC<Props> = ({origin, target}: Props) => {
       setTradeInformation({
         ...tradeInformation,
         hostAmount: '',
-        targetAmount: ''
+        targetAmount: '',
       });
       return;
     }
 
-    const {originToken, originToTarget} = swapInformation;
+    const { originToken, originToTarget } = swapInformation;
     const amount = new TokenAmount(
       originToken!,
       JSBI.multiply(
@@ -188,7 +212,9 @@ const Swap: FC<Props> = ({origin, target}: Props) => {
     );
     const trade = new Trade(originToTarget!, amount, TradeType.EXACT_INPUT);
 
-    const {priceImpactWithoutFee, realizedLPFee} = computeTradePriceBreakdown(trade);
+    const { priceImpactWithoutFee, realizedLPFee } = computeTradePriceBreakdown(
+      trade,
+    );
 
     setTradeInformation({
       trade,
@@ -212,12 +238,12 @@ const Swap: FC<Props> = ({origin, target}: Props) => {
       setTradeInformation({
         ...tradeInformation,
         hostAmount: '',
-        targetAmount: ''
+        targetAmount: '',
       });
       return;
     }
 
-    const {targetToken, targetToOrigin} = swapInformation;
+    const { targetToken, targetToOrigin } = swapInformation;
     const amount = new TokenAmount(
       targetToken!,
       JSBI.multiply(
@@ -226,7 +252,9 @@ const Swap: FC<Props> = ({origin, target}: Props) => {
       ),
     );
     const trade = new Trade(targetToOrigin!, amount, TradeType.EXACT_INPUT);
-    const {priceImpactWithoutFee, realizedLPFee} = computeTradePriceBreakdown(trade);
+    const { priceImpactWithoutFee, realizedLPFee } = computeTradePriceBreakdown(
+      trade,
+    );
 
     setTradeInformation({
       trade,
@@ -244,7 +272,7 @@ const Swap: FC<Props> = ({origin, target}: Props) => {
       setTradeInformation({
         ...tradeInformation,
         hostAmount: '',
-        targetAmount: ''
+        targetAmount: '',
       });
     };
   }, [fetchRouter, tradeInformation]);
@@ -253,64 +281,74 @@ const Swap: FC<Props> = ({origin, target}: Props) => {
     <>
       {loading && <span>Loading</span>}
       {swapInformation.originToTarget &&
-      swapInformation.targetToOrigin &&
-      !loading && (
-        <>
-          <label>
-            {`From (${origin.name}): `}
-            <br/>
-            <br/>
-            <input
-              value={tradeInformation.hostAmount}
-              type="text"
-              inputMode="decimal"
-              autoComplete="off"
-              autoCorrect="off"
-              placeholder="0.0"
-              minLength={1}
-              name="hostAmount"
-              onChange={(v) => handleHostAmountChange(v.target.value)}
-            />
-          </label>
-          <br/>
-          <br/>
-          <label>
-            {`To (${target.name}):`}
-            <br/>
-            <br/>
-            <input
-              value={tradeInformation.targetAmount}
-              type="text"
-              inputMode="decimal"
-              autoComplete="off"
-              autoCorrect="off"
-              placeholder="0.0"
-              minLength={1}
-              onChange={(v) => handleTargetAmountChange(v.target.value)}
-            />
-          </label>
-          {!tradeInformation.trade ? null : (
-            <>
-              <div>
-                {tradeInformation.trade.tradeType === TradeType.EXACT_INPUT ? (
-                  'Minimum received: ' +
-                  (computeSlippageAdjustedAmounts(tradeInformation.trade).min.toSignificant(4) ?? '-')) +
-                  tradeInformation.trade.outputAmount.currency.symbol
-                  : (
-                    'Maximum sold: ' +
-                    (computeSlippageAdjustedAmounts(tradeInformation.trade).max.toSignificant(4) ?? '-') +
-                    tradeInformation.trade.inputAmount.currency.symbol
-                  )
-                }
-              </div>
-              <div>Price Impact: {formattedPriceImpact(tradeInformation.priceImpactWithoutFee)}</div>
+        swapInformation.targetToOrigin &&
+        !loading && (
+          <>
+            <label>
+              {`From (${origin.name}): `}
+              <br />
+              <br />
+              <input
+                value={tradeInformation.hostAmount}
+                type="text"
+                inputMode="decimal"
+                autoComplete="off"
+                autoCorrect="off"
+                placeholder="0.0"
+                minLength={1}
+                name="hostAmount"
+                onChange={(v) => handleHostAmountChange(v.target.value)}
+              />
+            </label>
+            <br />
+            <br />
+            <label>
+              {`To (${target.name}):`}
+              <br />
+              <br />
+              <input
+                value={tradeInformation.targetAmount}
+                type="text"
+                inputMode="decimal"
+                autoComplete="off"
+                autoCorrect="off"
+                placeholder="0.0"
+                minLength={1}
+                onChange={(v) => handleTargetAmountChange(v.target.value)}
+              />
+            </label>
+            {!tradeInformation.trade ? null : (
+              <>
+                <div>
+                  {tradeInformation.trade.tradeType === TradeType.EXACT_INPUT
+                    ? 'Minimum received: ' +
+                      (computeSlippageAdjustedAmounts(
+                        tradeInformation.trade,
+                      ).min.toSignificant(4) ?? '-') +
+                      tradeInformation.trade.outputAmount.currency.symbol
+                    : 'Maximum sold: ' +
+                      (computeSlippageAdjustedAmounts(
+                        tradeInformation.trade,
+                      ).max.toSignificant(4) ?? '-') +
+                      tradeInformation.trade.inputAmount.currency.symbol}
+                </div>
+                <div>
+                  Price Impact:{' '}
+                  {formattedPriceImpact(tradeInformation.priceImpactWithoutFee)}
+                </div>
 
-              <div>Liquidity Provider
-                Fee: {tradeInformation.realizedLPFee ? tradeInformation.realizedLPFee?.toSignificant(6) + ' ' + tradeInformation.trade.inputAmount.currency.symbol : '-'}</div>
-            </>
-          )}
-        </>
-      )}
+                <div>
+                  Liquidity Provider Fee:{' '}
+                  {tradeInformation.realizedLPFee
+                    ? tradeInformation.realizedLPFee?.toSignificant(6) +
+                      ' ' +
+                      tradeInformation.trade.inputAmount.currency.symbol
+                    : '-'}
+                </div>
+              </>
+            )}
+          </>
+        )}
     </>
   );
 };
