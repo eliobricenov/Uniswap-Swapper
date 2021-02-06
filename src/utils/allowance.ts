4db9ade5ecc86df33185ea7bb6e4e19d2badc50b
Elio <eliobricenov@gmail.com>
2021-02-05 23:19:49 -0400

    add balance and allowance utils

---
+++ src/utils/allowance.ts
@@ -0,0 +1,43 @@
+import { Contract, BigNumber, providers } from 'ethers';
+import { erc20ABI } from '../contracts';
+
+export type AllowanceParams = {
+  tokenAddress: string;
+  signer: providers.JsonRpcSigner;
+  spenderAddress: string;
+  amount: string;
+};
+
+export const hasEnoughAllowance = async ({
+  tokenAddress,
+  signer,
+  spenderAddress,
+  amount,
+}: AllowanceParams) => {
+  const tokenContract = new Contract(tokenAddress, erc20ABI, signer);
+  const currentAllowance = await tokenContract.allowance(
+    signer.getAddress(),
+    spenderAddress
+  );
+
+  return BigNumber.from(currentAllowance).gte(BigNumber.from(amount));
+};
+
+export const approveTransactionAmount = async ({
+  tokenAddress,
+  signer,
+  amount,
+  spenderAddress,
+}: AllowanceParams) => {
+  const tokenContract = new Contract(tokenAddress, erc20ABI, signer);
+  const amountToAllow = BigNumber.from(amount).toHexString();
+  await tokenContract.approve(spenderAddress, amountToAllow);
+};
+
+export const approveIfRequired = async (params: AllowanceParams) => {
+  const hasEnough = await hasEnoughAllowance(params);
+  console.log('approveIfRequired ~ hasEnough', hasEnough);
+  if (!hasEnough) {
+    await approveTransactionAmount(params);
+  }
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,15 @@
-export * from './uniswap.utils';
+import { AllowanceParams, approveIfRequired } from './allowance';
+import { BalanceParams, checkBalance } from './balance';
+
+export * from './uniswap';
+export * from './allowance';
+export * from './balance';
+
+export const validateTransaction = async (
+  params: AllowanceParams & BalanceParams
+) => {
+  return Promise.all([
+    checkBalance({ ...params }),
+    approveIfRequired({ ...params }),
+  ]);
+};
